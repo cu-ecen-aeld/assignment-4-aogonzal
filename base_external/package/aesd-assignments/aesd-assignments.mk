df994003d20e5adc768310b078c2e8498a97ff40
aogonzal <aogonzal@gmail.com>
2022-11-23 08:55:24 -0600

    commit hash

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = c8fb85db652c0289995e1d1b7fc5da55fdc7dcf3
+AESD_ASSIGNMENTS_VERSION = 818d7704f0c685ab4ce3cf1d9d26042fbb835772
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
